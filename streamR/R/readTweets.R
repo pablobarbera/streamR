44aaa565e502d056c8e72e31966bc74e28d46b00
expectopatronum <veroamilbe@gmail.com>
2016-06-12 08:05:53 +0200

    replaces cat by message

--- streamR/R/readTweets.R
+++ streamR/R/readTweets.R
@@ -80,7 +80,7 @@ readTweets <- function(tweets, verbose=TRUE){
     }
               
     # information message
-    if (verbose==TRUE) cat(length(results.list), "tweets have been parsed.", "\n")
+    if (verbose==TRUE) message(length(results.list), " tweets have been parsed.", "\n")
     return(results.list)
 }
 
